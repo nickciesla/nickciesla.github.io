819060824ade2649b662f33977ce6389c2940662
nickciesla <nicholasciesla@gmail.com>
2021-09-20 22:00:44 -0400

    Update boxscore.js

--- js/boxscore.js
+++ js/boxscore.js
@@ -10,7 +10,15 @@ function convert_date(old_date) {
 
 function get_scores(week) {
 
-  $("#scores").empty();
+  $("#thursday").empty();
+  $("#sunday").empty();
+  $("#monday").empty();
+  $("#bets").empty();
+
+  $("#thursday").append('<div class="row text-center align-items-center"> <div class="col-sm"><h1><span class="badge badge-primary">Thursday</span></h1></div><div class="col-sm"></div><div class="col-sm"></div></div>')
+  $("#sunday").append('<div class="row text-center align-items-center"> <div class="col-sm"><h1><span class="badge badge-primary">Sunday</span></h1></div><div class="col-sm"></div><div class="col-sm"></div></div>')
+  $("#monday").append('<div class="row text-center align-items-center"> <div class="col-sm"><h1><span class="badge badge-primary">Monday</span></h1></div><div class="col-sm"></div><div class="col-sm"></div></div>')
+
   $.getJSON('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?week=' + week, function(data) {
     var total = 0
     var total1 = 0
@@ -35,15 +43,12 @@ function get_scores(week) {
           details = data.events[i].competitions[0].situation.lastPlay.text
         } else {
           if (typeof data.events[i].competitions[0].headlines != 'undefined') {
-          details = data.events[i].competitions[0].headlines[0].shortLinkText
-        } else {
-          details = ''
-        }
+            details = data.events[i].competitions[0].headlines[0].shortLinkText
+          } else {
+            details = ''
+          }
         }
       }
-
-      //$('#thursday').append('<label>' + merica_date.getDay() + '</label><label>' + data.events[i].name + '</label>');
-
 
       text += data.events[i].competitions[0].competitors[1].team.abbreviation + ' ';
       text += data.events[i].competitions[0].competitors[1].score + ' - ';
@@ -53,7 +58,38 @@ function get_scores(week) {
       var team1 = data.events[i].competitions[0].competitors[1].team.logo
       var team2 = data.events[i].competitions[0].competitors[0].team.logo
 
-      $('#scores').append('<div class="row text-center border-top border-left border-right align-items-center"><div class="col-sm"><label class="boxscore">' + details + '</label></div></div><div class="row text-center border-bottom border-left border-right align-items-center"><div class="col-sm"><img src="' + team1 + '" width="80" height="80"></div><div class="col-sm"><label class="boxscore">' + text + '</label></div><div class="col-sm"><img src="' + team2 + '" width="80" height="80"></div></div>');
+      var appendText = '<div class="row text-center border-top border-left border-right align-items-center"><div class="col-sm"><label class="boxscore">' + details + '</label></div></div><div class="row text-center border-bottom border-left border-right align-items-center">'
+
+      if (typeof data.events[i].competitions[0].competitors[1].winner != 'undefined') {
+        if (data.events[i].competitions[0].competitors[1].winner) {
+          appendText += '<div class="col-sm"><h3><label class="badge badge-success">W</label></h3></div>'
+        } else {
+          appendText += '<div class="col-sm"><h3><label class="badge badge-danger">L</label></h3></div>'
+        }
+      }
+
+      appendText += '<div class="col-sm"><img src="' + team1 + '" width="80" height="80"></div><div class="col-sm"><label class="boxscore">' + text + '</label></div><div class="col-sm"><img src="' + team2 + '" width="80" height="80"></div>'
+
+      if (typeof data.events[i].competitions[0].competitors[0].winner != 'undefined') {
+        if (data.events[i].competitions[0].competitors[0].winner) {
+          appendText += '<div class="col-sm"><h3><label class="badge badge-success">W</label></h3></div>'
+        } else {
+          appendText += '<div class="col-sm"><h3><label class="badge badge-danger">L</label></h3></div>'
+        }
+      }
+
+      appendText += '</div>'
+
+      if (merica_date.getDay() == 0) {
+        $('#sunday').append(appendText);
+      }
+      if (merica_date.getDay() == 1) {
+        $('#monday').append(appendText);
+      }
+      if (merica_date.getDay() == 4) {
+        $('#thursday').append(appendText);
+      }
+
 
 
       if (merica_date.getDay() == 0) {
@@ -74,19 +110,17 @@ function get_scores(week) {
 
     }
 
-    $('#scores').append('<div class="row text-center align-items-center"><div class="col-sm"></div><div class="col-sm"><label class="boxscore">Caesars Total Over Under: ' + totalOverUnder + '</label><label class="boxscore">Hanson Bet Total: ' + total + '</label><label class="boxscore">1:00 PM Total: ' + total1 + '</label><label class="boxscore">4:00 PM Total: ' + total4 + '</label></div><div class="col-sm"></div></div>');
+    $('#bets').append('<div class="row text-center align-items-center"><div class="col-sm"></div><div class="col-sm"><label class="boxscore">Caesars Total Over Under: ' + totalOverUnder + '</label><label class="boxscore">Hanson Bet Total: ' + total + '</label><label class="boxscore">1:00 PM Total: ' + total1 + '</label><label class="boxscore">4:00 PM Total: ' + total4 + '</label></div><div class="col-sm"></div></div>');
   })
 }
 
 $(document).ready(function() {
   $.getJSON('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard', function(data) {
     $('#week a').on('click', function() {
-      var txt = ($(this).text());
-      get_scores(txt)
+      get_scores(($(this).text()))
     });
 
-    $('#week_label').text(data.week.number)
     curWeek = data.week.number
-    get_scores(data.week.number)
+    get_scores(curWeek)
   })
 })
