fe5d20892a3043dfb96b6249a3fe747aea800bc3
nickciesla <nicholasciesla@gmail.com>
2023-04-08 11:59:17 -0400

    add cut
    add cut

--- masters/js/masters.js
+++ masters/js/masters.js
@@ -1,7 +1,8 @@
 //masters
 const scores = Object() 
 const teams = Object()
-const withdrawn = ['K. Na', 'W. Zalatoris']
+const withdrawn = ['K. Na', 'W. Zalatoris', 'L. Oosthuizen']
+const cut = 53
 
 teams['Voor'] = ['R. McIlroy', 'J. Rahm', 'T. Hoge', 'T. Kim', 'K. Kitayama', 'H. English']
 teams['KG'] = ['S. Scheffler', 'J. Rahm', 'A. Scott', 'S. Theegala', 'H. Varner III', 'C. Schwartzel']
@@ -15,12 +16,14 @@ teams['Doug'] = ['P. Cantlay', 'X. Schauffele', 'T. Hoge', 'B. Harman', 'K.H. Le
 teams['Tim'] = ['J. Rahm', 'C. Smith', 'T. Kim', 'S. Kim', 'K. Kitayama', 'H. English']
 
 $(document).ready(function() {
+  
   $.getJSON('https://site.api.espn.com/apis/site/v2/sports/golf/pga/scoreboard', function(data) {
 
     for (let i = 0; i < 88; i++) {
       ath = data.events[0].competitions[0].competitors[i]
       golfer = ath.athlete.shortName
       score = ath.score
+      pos = i + 1
 
       if (typeof ath.linescores[0] != 'undefined') {
         if (typeof ath.linescores[0].value != 'undefined') {
@@ -61,30 +64,44 @@ $(document).ready(function() {
       } else {
         day4 = 77
       }
-      scores[golfer] = [score, day1, day2, day3, day4]
+      scores[golfer] = [pos, score, day1, day2, day3, day4]
+    }
+    console.log(scores)
+
+    for (let [golfer, score] of Object.entries(scores)) {
+      if (withdrawn.includes(golfer)) {
+        score = ['+20', 77, 77, 77, 77]
+      }
+      if (score[0] > cut && !withdrawn.includes(golfer)) {
+        score[1] = '+' + (parseInt(score[1]) + 10)
+      }
     }
 
     for (let [name, golfers] of Object.entries(teams)) {
       $("#teamscores").append('<div class="row text-center align-items-center"><h1><span class="badge badge-md">' + name + '</span></h1></div>')
       score_count = 0
       score_total = 0
-      for (let [golfer, score] of Object.entries(scores)) {
+      for (var [golfer, score] of Object.entries(scores)) {
         wrote = false
         if (golfers.includes(golfer)) {
-          if (withdrawn.includes(golfer)) {
-            score = ['+100', 100, 100, 100, 100]
-          }
           if (score_count < 4) {
-            if (score[0] != 'E') {
-              score_total = score_total + parseInt(score[0])
+            if (score[1] != 'E') {
+              score_total = score_total + parseInt(score[1])
             }
             score_count = score_count + 1
-            $("#teamscores").append('<div class="row text-center align-items-center"><div class="col-md text-truncate">' + golfer + '</div><div class="col-md">' + score + '</div></div>')
+            $("#teamscores").append('<div class="row text-center align-items-center"><div class="col-md text-truncate">' + golfer + '</div><div class="col-md">' + score.slice(1) + '</div></div>')
             wrote = true
           }
           if (score_count == 4 && !wrote) {
-            $("#teamscores").append('<div class="row text-center align-items-center"><div class="col-md text-truncate"><s>' + golfer + '</s></div><div class="col-md"><s>' + score + '</s></div></div>')
+            $("#teamscores").append('<div class="row text-center align-items-center"><div class="col-md text-truncate"><s>' + golfer + '</s></div><div class="col-md"><s>' + score.slice(1) + '</s></div></div>')
           }
+        }
+      }
+      if (score_total >= 0) {
+        if (score_total == 0) {
+          score_total = 'E'
+        } else {
+          score_total = '+' + score_total
         }
       }
       $("#teamscores").append('<div class="row text-center align-items-center"><div class="col-md text-truncate">Total</div><div class="col-md">' + score_total + '</div></div>')
